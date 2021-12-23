3ee1234a8bc25b6640b50d19e820011cfd35a3ae
Marley Doughty <marleydoughty@yahoo.com>
2021-12-23 19:43:25 +0000

    installed packages and created database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,45 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"username" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"password" TEXT NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."folders" (
+	"folderId" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"userId" int NOT NULL,
+	CONSTRAINT "folders_pk" PRIMARY KEY ("folderId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."icons" (
+	"iconId" serial NOT NULL,
+	"folderId" int,
+	"name" TEXT NOT NULL,
+	"url" TEXT NOT NULL,
+	CONSTRAINT "icons_pk" PRIMARY KEY ("iconId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "folders" ADD CONSTRAINT "folders_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+ALTER TABLE "icons" ADD CONSTRAINT "icons_fk0" FOREIGN KEY ("folderId") REFERENCES "folders"("folderId");
