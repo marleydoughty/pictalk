48c892bb97d807b70db4977c08f0f19fe932e69f
Marley Doughty <marleydoughty@yahoo.com>
2022-01-09 00:47:47 +0000

    adjusted styling of folders page

--- client/components/folder-items.jsx
+++ client/components/folder-items.jsx
@@ -33,7 +33,7 @@ export default class FolderItems extends React.Component {
   render() {
     const folders = this.state.folders;
     const allFolders = folders.map(folder => (
-      <div key={folder.folderId} onClick={ () => this.openFolder(folder.folderId)}>
+      <div className='flex-basis' key={folder.folderId} onClick={ () => this.openFolder(folder.folderId)}>
         <div className='folders-container'>
           <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/OneDrive_Folder_Icon.svg/2048px-OneDrive_Folder_Icon.svg.png'></img>
           <p>{folder.name}</p>
