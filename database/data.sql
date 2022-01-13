150be5905c343c20e6b32e9b8ce5f8ef9129ae07
Marley Doughty <marleydoughty@yahoo.com>
2022-01-13 23:30:04 +0000

    adjusted layout of icons, added more buttons to home page, and inserted guest login into database

--- database/data.sql
+++ database/data.sql
@@ -1,5 +1,6 @@
 insert into "users" ( "username", "hashedPassword")
-values ('data2','$argon2i$v=19$m=4096,t=3,p=1$Nhjgyfvhguhjhhjg$');
+values ('data2','$argon2i$v=19$m=4096,t=3,p=1$Nhjgyfvhguhjhhjg$'),
+      ('guest','$argon2i$v=19$m=4096,t=3,p=1$GyYkCX/4+kIEz/TeV8G52Q$aHhtlPgcTQeuSki/iLB5IAMe8mXFJb4y4z/zBc6lzWY');
 
 insert into "folders" ("userId", "name")
 values ('1','home'), ('1','self-help'),('1','food'), ('1','people'), ('1','feelings'),('1','toys'), ('1','animals'), ('1','shapes'), ('1','clothes'), ('1', 'places');
@@ -9,8 +10,8 @@ values ('1','I','/icons/I.png'),('1','want','/icons/want.png'),('1','help','/ico
       ('1','please','/icons/please.png'),('1','thanks','/icons/thanks.png'),('1','bad','/icons/bad.png'),('1','good','/icons/good.png'),
       ('1','more','/icons/more.png'),('1','feel','/icons/feel.png'),
       ('1','give','/icons/give.png'),('1','eat','/icons/eat.png'),('1','food','/icons/food.png'),
-      ('1','go','/icons/go.png'),('1','stop','/icons/stop.png'),
-      ('1','toys','/icons/toys.png'),('1','you','/icons/you.png'),
+      ('1','go','/icons/go.png'),('1','stop','/icons/stop.png'),('1','potty','/icons/potty.png'),
+      ('1','toys','/icons/toys.png'),('1','you','/icons/you.png'), ('1', 'get dressed', '/icons/get-dressed.png'), ('1','watch tv','/icons/watch-tv.png'),
       ('2','potty','/icons/potty.png'),('2','wash-hands','/icons/wash-hands.png'),('2','shower','/icons/shower.png'),
       ('3','burrito','/icons/burrito.png'),('3','cheese','/icons/cheese.png'),('3', 'nuggets','/icons/chicken-nuggets.png'),
       ('3','chips','/icons/chips.png'),('3','cookie','/icons/cookie.png'), ('3','crackers','/icons/crackers.png'), ('3','grilled cheese','/icons/grilled-cheese.png'),
@@ -28,5 +29,5 @@ values ('1','I','/icons/I.png'),('1','want','/icons/want.png'),('1','help','/ico
       ('8','star','/icons/star.png'),('8','triangle','/icons/triangle.png'),('8','oval','/icons/oval.png'),
       ('9','hat','/icons/hat.png'), ('9','beanie','/icons/beanie.png'), ('9','shirt','/icons/shirt.png'),('9','pants','/icons/pants.png'),
       ('9','dress','/icons/dress.png'), ('9','jacket','/icons/jacket.png'), ('9','shoes','/icons/shoes.png'),('9','socks','/icons/socks.png'),
-      ('10', 'bathroom','/icons/bathroom.png'),('10', 'bedroom','/icons/bedroom.png'),('10', 'kitchen','/icons/kitchen.png'),('10','living room','/icons/living-room.png'),
-      ('10', 'park','/icons/park.png'),('10', 'school','/icons/school.png')
+      ('10','bathroom','/icons/bathroom.png'),('10', 'bedroom','/icons/bedroom.png'),('10', 'kitchen','/icons/kitchen.png'),('10','living room','/icons/living-room.png'),
+      ('10','park','/icons/park.png'),('10','school','/icons/school.png')
