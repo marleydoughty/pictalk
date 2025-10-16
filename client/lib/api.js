1329ad1e29b7a50be26d713642fdd3b44128b53a
Marley Doughty <marleydoughty@Mac.lan>
2025-10-15 20:03:46 -0700

    removed unused file and comments

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Create axios instance with default config
 const apiClient = axios.create({
   baseURL: '/api',
   headers: {
@@ -8,7 +7,6 @@ const apiClient = axios.create({
   }
 });
 
-// Request interceptor to add auth token
 apiClient.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
@@ -22,7 +20,6 @@ apiClient.interceptors.request.use(
   }
 );
 
-// Response interceptor for error handling
 apiClient.interceptors.response.use(
   response => response.data,
   error => {
@@ -33,13 +30,10 @@ apiClient.interceptors.response.use(
 );
 
 export const api = {
-  // Folders
   getFolders: () => apiClient.get('/folders'),
 
-  // Icons
   getIcons: (folderId = 1) => apiClient.get(`/icons/${folderId}`),
 
-  // Auth
   signIn: credentials => apiClient.post('/auth/sign-in', credentials),
 
   signUp: credentials => apiClient.post('/auth/sign-up', credentials)
